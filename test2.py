da85b4e51cab9dfbf18dfe8f228796df973966cd
viktor <victorcska5@mail.ru>
2023-05-04 21:58:26 +0800

    init

--- test2.py
+++ test2.py
@@ -5,6 +5,8 @@ bot = telebot.TeleBot('5680128942:AAGvcEmSyNObpoV9DyV4YyqsoGei1hanAsc')
 API = '86e8cbac4d64f630c00a528611932773'
 
 
+""" bot for checking weather """
+
 @bot.message_handler(commands=['start'])
 def start(message):
     mess = f'<b>Hello  {message.from_user.first_name}</b>  <u>enter command /time</u>'
