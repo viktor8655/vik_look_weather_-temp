a070497f7ea15334757bcb1a0db79fd43c3d9578
viktor <victorcska5@mail.ru>
2023-05-06 21:17:54 +0800

    done

--- test2.py
+++ test2.py
@@ -17,9 +17,16 @@ def start(message):
 def get_weather(message):
     city = message.text.strip().lower()
     res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric')
-    data = json.loads(res.text)
-    temp = data['main']['temp']
-    bot.reply_to(message, f"Now the weather:{temp}")
+    if res.status_code == 200:
+        data = json.loads(res.text)
+        temp = data['main']['temp']
+        bot.reply_to(message, f"Now the weather:{temp}")
+
+        image = 'sunny.jpeg' if 10.0 < temp > 10.0  else 'cold.png'
+        file = open('./' + image, 'rb')
+        bot.send_photo(message.chat.id, file)
+    else:
+        bot.reply_to(message, " wrong city")
 
 
 bot.polling(none_stop=True)
