083144023061d4238b402f8d91e4b85e377a4bc5
viktor <victorcska5@mail.ru>
2023-05-06 21:03:34 +0800

    almost done

--- test2.py
+++ test2.py
@@ -7,12 +7,19 @@ bot = telebot.TeleBot('5680128942:AAGvcEmSyNObpoV9DyV4YyqsoGei1hanAsc')
 API = '86e8cbac4d64f630c00a528611932773'
 
 
-
-
 @bot.message_handler(commands=['start'])
 def start(message):
-    mess = f'<b>Hello  {message.from_user.first_name}</b>  <u>enter command /time</u>'
+    mess = f'<b>Hello  {message.from_user.first_name}</b>  <u>write the name of the city</u>'
     bot.send_message(message.chat.id, mess, parse_mode='html')
+
+
+@bot.message_handler(content_types=['text'])
+def get_weather(message):
+    city = message.text.strip().lower()
+    res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric')
+    data = json.loads(res.text)
+    temp = data['main']['temp']
+    bot.reply_to(message, f"Now the weather:{temp}")
 
 
 bot.polling(none_stop=True)
